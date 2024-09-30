d9da3d93726639efcb280e2f0c9f24be86137851
Shubham <shubham@gmail.com>
2024-09-30 14:33:54 +0530

    Add delete_files function

--- problem_01.cjs
+++ problem_01.cjs
@@ -20,29 +20,48 @@ function make_directory(dirPath) {
 
 function json_file_generator(count, dirPath) {
     const promise_values = [];
-    
-    
+
+
     for (let index = 1; index <= count; index++) {
         const json_file = path.join(dirPath, `json_file_${index}.json`);
         const json_data = JSON.stringify({
             username: 'shubham',
             gender: 'male'
-        });       
+        });
 
         const p = new Promise((resolve, reject) => {
-            fs.writeFile(json_file, json_data, (err, file) => {
+            fs.writeFile(json_file, json_data, (err) => {
                 if (err) {
                     reject(err);
                 } else {
-                    resolve(file);
+                    resolve(json_file);
                 }
             });
         });
         promise_values.push(p);
 
     }
+     
     return Promise.all(promise_values)
 }
 
 
-module.exports = { make_directory, json_file_generator };
+function delete_files(files) {
+       
+    const remove_files = files.map((link) => {
+        new Promise((resolve, reject) => {
+            fs.unlink(link, (err) => {
+                if (err) {
+                    reject(err);
+                }else{
+                    resolve("success")
+                }
+            })
+        });
+    });
+      
+    return Promise.all(remove_files);
+}
+
+
+module.exports = { make_directory, json_file_generator, delete_files };
