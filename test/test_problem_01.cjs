e21f251ceaad2afea01ab15afeb6139ff80b8ea9
Shubham <shubham@gmail.com>
2024-10-02 10:28:17 +0530

    Add mainfunction

--- test/test_problem_01.cjs
+++ test/test_problem_01.cjs
@@ -1,26 +1,9 @@
-const path = require("path");
-
-const { make_directory, json_file_generator, delete_files } = require("../problem_01.cjs");
+const { problem_01_async_function } = require("../problem_01.cjs");
 
 
-const folder = path.join(__dirname, "../json-files");
-
-async function problem_01_process(count) {
-    try {
-        await make_directory(folder);
-        console.log("Directory created..!");
-
-        const json_file = await json_file_generator(count, folder);      
-        console.log("json files generated..!");
-
-        await delete_files(json_file);
-        console.log("Deleted successfully..!");
-        
-
-    } catch (error) {
-        console.log("Something is wrong..!", error);
-
-    }
+function main() {
+    
+    problem_01_async_function(4);
 }
 
-problem_01_process(4);
+main();
